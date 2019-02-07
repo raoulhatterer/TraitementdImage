8300f1f637f0cab9a6ed3269a161b1f793295a5e
raoulhatterer <hatterer.raoul@gmail.com>
2019-02-07 14:40:13 +0100

    exports

--- mystere.py
+++ mystere.py
@@ -4,10 +4,10 @@ largeur_image=480
 hauteur_image=300
 for y in range(hauteur_image):
     for x in range(largeur_image):
-        r,v,b=img.getpixel((x,y))
-        n_r=v
-        n_v=b
-        n_b=r
-        img.putpixel((x,y),(n_r,n_v,n_b))
+        rouge,vert,bleu=img.getpixel((x,y))
+        nouveau_rouge=vert
+        nouveau_vert=bleu
+        nouveau_bleu=rouge
+        img.putpixel((x,y),(nouveau_rouge,nouveau_vert,nouveau_bleu))
 img.show()
 img.save("pommeMystere.jpg")
--- pommegrise.py
+++ pommegrise.py
@@ -4,10 +4,10 @@ largeur_image=480
 hauteur_image=300
 for y in range(hauteur_image):
     for x in range(largeur_image):
-        r,v,b=img.getpixel((x,y))
-        n_r=(v+b+r)//3
-        n_v=(v+b+r)//3
-        n_b=(v+b+r)//3
-        img.putpixel((x,y),(n_r,n_v,n_b))
+       rouge,vert,bleu=img.getpixel((x,y))
+       nouveau_rouge=(vert+bleu+rouge)//3
+       nouveau_vert=(vert+bleu+rouge)//3
+       nouveau_bleu=(vert+bleu+rouge)//3
+       img.putpixel((x,y),(nouveau_rouge,nouveau_vert,nouveau_bleu))
 img.show()
 img.save("pommegrise.jpg")
